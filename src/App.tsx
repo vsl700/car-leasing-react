2f1183bddfdb97c086ce10386a2de79c06a071da
vsl700 <51147745+vsl700@users.noreply.github.com>
2024-06-13 16:49:15 +0300

    Checkpoint commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Car Leasing Calculator</h1>
+            <div className='Calc-input'>
+                <InputContainer>
+                    <label htmlFor="car-type">Car Type:</label>
+                    <select id='car-type'>
+                        <option value="brand-new">Brand New</option>
+                        <option value="used">Used</option>
+                    </select>
+                    <label htmlFor='car-value'>Car Value (€10,000 - €200,000):</label>
+                    <input type="text" defaultValue={120000} />
+                </InputContainer>
+                <InputContainer>
+                    <label htmlFor="car-type">Car Type:</label>
+                    <select id='car-type'>
+                        <option value="brand-new">Brand New</option>
+                        <option value="used">Used</option>
+                    </select>
+                </InputContainer>
+            </div>
+            <hr />
+        </div>
+    );
+}
+
+// An element that contains part of the inputs.
+// It is used to either arrange multiple InputContainer-s
+// horizontally or vertically
+const InputContainer = (props: {children: React.ReactElement | React.ReactElement[]}) => {
+    return (
+        <div className='Input-container'>
+            {props.children}
+        </div>
+    )
 }
 
 export default App;
